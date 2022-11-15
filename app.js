fa9466a623520164a16bde577e70658559b154df
Haw Haw Tan <thawhaw@gmail.com>
2022-11-15 00:45:51 -0800

    added overlay without info

--- app.js
+++ app.js
@@ -11,3 +11,22 @@ const observer = new IntersectionObserver((lists) =>{
 
 const hidden = document.querySelectorAll('.hidden');
 hidden.forEach((el) => observer.observe(el));
+
+function on() {
+   // document.getElementById("overlay").style.left = "0";
+   // document.getElementById("overlay").style.bottom = "0";
+   document.getElementById("overlay").style.opacity = "0.5";
+   document.getElementById("overlay").style.zIndex = "1";
+ }
+ 
+ function off() {
+   document.getElementById("overlay").style.opacity = "0";
+   document.getElementById("overlay").style.zIndex = "-1";
+   // document.getElementById("overlay").style.zIndex = "none";
+   // document.getElementById("overlay").style.zIndex = "unset";
+   // document.getElementById("overlay").style.left = "100%";
+   // document.getElementById("overlay").style.bottom = "100%";
+   // document.getElementById("overlay").style.animation = "reverse-pop-contact 1s ease-in-out";
+   // document.getElementById("overlay").style.transform = "scale(0)";
+
+ }
